a0724f66d5037f5a87f568704cdcf1e3d83783ee
Stas SUSHKOV <stas@net.utcluj.ro>
2009-09-07 14:15:13 +0300

    Fix for issue #1

--- php-api/jQueryAnnotate.class.php
+++ php-api/jQueryAnnotate.class.php
@@ -1,76 +1,127 @@
 <?php
-	class jQueryAnnotate {
+/**
+ * PHP API for jQuery Image Annotation Plugin
+ * @author Stas SUSHKOV <stas@nerd.ro>
+ * @version 0.2
+ * @since PHP 5.2
+ */
 
-		function __construct($filename = "data.csv") {
-			$this->filename = $filename;
-			$this->file = @fopen($filename, "r+");
-			$this->items = $this->csv_to_array();
-		}
+class jQueryAnnotate {
 
-		function get() {
-			$items = $this->items;
-			echo "[\n";
-			foreach($items as $item) {
-				if($item) {
-					$a = explode("\t", $item);
-					echo $this->format($a);
-				}
+	/**
+	 * Constructor
+	 * Opens and reads items from the data file
+	 *
+	 * @param string $filename of the data file, default is 'data.csv'
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function __construct($filename = "data.csv") {
+		$this->filename = $filename;
+		$this->file = @fopen($filename, "r+");
+		$this->items = $this->csv_to_array();
+	}
+
+	/**
+	 * Outputs data file content in a JSON format
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function get() {
+		$items = $this->items;
+		echo "[\n";
+		foreach($items as $item) {
+			if($item) {
+				$a = explode("\t", $item);
+				echo $this->format($a);
 			}
-			echo "]\n";
 		}
+		echo "]\n";
+	}
 
-		function save() {
-			$data = array(
-				$_GET["top"],  
-				$_GET["left"],  
-				$_GET["width"],  
-				$_GET["height"], 
-				$_GET["text"],  
-				"id_".md5($_GET["text"]),
-				"true"
-			);
+	/**
+	 * Adds a new annotation into the data file
+	 * All HTML and special characters are stripped out
+	 * As a result outputs the new saved id of the annotation
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function save() {
+		$data = array(
+			$_GET["top"],  
+			$_GET["left"],  
+			$_GET["width"],  
+			$_GET["height"], 
+			$this->html2txt($_GET["text"]),  
+			"id_".md5($_GET["text"]),
+			"true"
+		);
 
-			if( file_put_contents( $this->filename, implode("\t",$data)."\n", FILE_APPEND | LOCK_EX ))
-				echo '{ "annotation_id": "id_'.md5($_GET["text"]).'" }';
-		}
+		if($data[4] && file_put_contents( $this->filename, implode("\t",$data)."\n", FILE_APPEND | LOCK_EX ))
+			echo '{ "annotation_id": "id_'.md5($_GET["text"]).'" }';
+	}
 
-		function delete() {
-			$to_delete = array(
-				$_GET["top"],
-				$_GET["left"],
-				$_GET["width"],
-				$_GET["height"],
-				$_GET["text"],
-				$_GET["id"],
-			);
+	/**
+	 * Deletes an annotation from the data file
+	 * As a result outputs a 200 HTTP header
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function delete() {
+		$to_delete = array(
+			$_GET["top"],
+			$_GET["left"],
+			$_GET["width"],
+			$_GET["height"],
+			$_GET["text"],
+			$_GET["id"],
+		);
 
-			$items = $this->items;
-			$i = 0;
-			foreach($items as $item) {
-				if($item) {
-					$item = explode("\t", $item);
-				        if(($item[5] == $to_delete[5]) && ($item[6] == "true\n"))
-						unset($items[$i]);
-				}
-				$i++;
+		$items = $this->items;
+		$i = 0;
+		foreach($items as $item) {
+			if($item) {
+				$item = explode("\t", $item);
+				if(($item[5] == $to_delete[5]) && ($item[6] == "true\n"))
+					unset($items[$i]);
 			}
-			if( file_put_contents( $this->filename, implode("",$items), LOCK_EX ))
-				header("HTTP/1.0 200 OK");
+			$i++;
 		}
+		if( file_put_contents( $this->filename, implode("",$items), LOCK_EX ))
+			header("HTTP/1.0 200 OK");
+	}
 
-		function csv_to_array() {
-			if($this->file) {
+	/**
+	 * Converts the csv content from data file to an array
+	 *
+	 * @access public
+	 * @return array
+	 */
+	function csv_to_array() {
+		if($this->file) {
 			$csv_array = array();
-				while (!feof($this->file)) {
-					$csv_array[] = fgets($this->file);
-				}
+			while (!feof($this->file)) {
+				$csv_array[] = fgets($this->file);
 			}
-			else
-				$this->print_error("Could not open file.");
-			return $csv_array;
 		}
+		else
+			$this->print_error("Could not open file.");
+		return $csv_array;
+	}
 
-		function format($t) {
+	/**
+	 * Converts an array to a JSON required format
+	 *
+	 * @param array $t item from the data file content array
+	 * @access public
+	 * @return string
+	 */
+	function format($t) {
+		if(is_array($t) && count($t) == 7)
 			return "
 				{
 					\"top\":	$t[0],
@@ -82,14 +133,57 @@
 					\"editable\":	$t[6]
 				},
 			";
-		}
+	}
 
-		function print_error($m) {
-			echo "<pre>$m</pre>\n";
-		}
+	/**
+	 * Strip a string from unwanted tags and special characters
+	 *
+	 * @param string $text to be cleared
+	 * @access public
+	 * @return string
+	 */
+	function html2txt($text) {
+		$search = array ('@<script[^>]*?>.*?</script>@si',	// Strip out javascript
+				 '@<[\/\!]*?[^<>]*?>@si',		// Strip out HTML tags
+				 '@([\r\n])[\s]+@',			// Strip out white space
+				 '@&(quot|#34);@i',			// Replace HTML entities
+				 '@&(lt|#60);@i',
+				 '@&(gt|#62);@i',
+				 '@&(nbsp|#160);@i',
+				 '@&#(\d+);@e');			// evaluate as php
 
-		function __destruct() {
-			fclose($this->file);
-		}
-	}	
+		$replace = array ('',
+				 '',
+				 '\1',
+				 '"',
+				 '<',
+				 '>',
+				 ' ',
+				 'chr(\1)');
+
+		return trim(preg_replace($search, $replace, $text));
+	}
+
+	/**
+	 * Prints a <pre> formated error message
+	 *
+	 * @param string $m message to be printed as an error
+	 * @access public
+	 * @return string
+	 */
+	function print_error($m) {
+		die("<pre>$m</pre>\n");
+	}
+
+	/**
+	 * Destructor
+	 * Closes the file the constructor opened.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function __destruct() {
+		fclose($this->file);
+	}
+}	
 ?>
